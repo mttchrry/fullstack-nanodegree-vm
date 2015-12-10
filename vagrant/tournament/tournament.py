dbbbeb87d68095be9fa998fec231afd1d3ccebbc
unknown <mcherry@USMAYMCHERRYPC2.ra-int.com>
2015-12-10 15:12:13 -0500

    Ready for first submission, cleaned up the tables to non-duplicating information, formatting compliant

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# 
+#
 # tournament.py -- implementation of a Swiss-system tournament
 #
 
@@ -17,16 +17,18 @@ def deleteMatches():
     c = DB.cursor()
     """ delete the match data """
     c.execute("DELETE FROM matches")
-    DB.commit() 
+    DB.commit()
     DB.close()
+
 
 def deletePlayers():
     """Remove all the player records from the database."""
     DB = connect()
     c = DB.cursor()
     c.execute("DELETE FROM players")
-    DB.commit() 
+    DB.commit()
     DB.close()
+
 
 def countPlayers():
     """Returns the number of players currently registered."""
@@ -34,30 +36,32 @@ def countPlayers():
     c = DB.cursor()
     c.execute("SELECT count(*) FROM players")
     records = c.fetchone()
-    DB.commit() 
+    DB.commit()
     DB.close()
     return records[0]
 
+
 def registerPlayer(name):
     """Adds a player to the tournament database.
-  
+
     The database assigns a unique serial id number for the player.  (This
     should be handled by your SQL database schema, not in your Python code.)
-  
+
     Args:
       name: the player's full name (need not be unique).
     """
     DB = connect()
     c = DB.cursor()
     c.execute("INSERT INTO players (name) VALUES (%s)", (name,))
-    DB.commit() 
+    DB.commit()
     DB.close()
+
 
 def playerStandings():
     """Returns a list of the players and their win records, sorted by wins.
 
-    The first entry in the list should be the player in first place, or a player
-    tied for first place if there is currently a tie.
+    The first entry in the list should be the player in first place, or a
+    player tied for first place if there is currently a tie.
 
     Returns:
       A list of tuples, each of which contains (id, name, wins, matches):
@@ -68,17 +72,22 @@ def playerStandings():
     """
     DB = connect()
     c = DB.cursor()
-    # slightly complex query returns (player.id, player.name, calculated player wins, calculated player matches)
-    # by joining together the players and matches tables and doing counts on them. 
-    c.execute('''SELECT players.id, players.name, 
-        count(case when players.id = matches.winner then 1 else null end) as wins,
-        count(case when players.id = matches.winner or players.id = matches.loser then 1 else null end) as matchCount
-        FROM players LEFT OUTER JOIN matches on players.id in (matches.winner, matches.loser) 
+    # slightly complex query returns (player.id, player.name,
+    # calculated player wins, calculated player matches) by joining
+    # together the players and matches tables and doing counts on them.
+    c.execute('''SELECT players.id, players.name,
+        count(case when players.id = matches.winner then 1
+            else null end) as wins,
+        count(case when players.id = matches.winner or
+            players.id = matches.loser then 1 else null end) as matchCount
+        FROM players LEFT OUTER JOIN matches on players.id in
+        (matches.winner, matches.loser)
         GROUP BY players.id ORDER BY wins DESC''')
     records = c.fetchall()
-    DB.commit() 
+    DB.commit()
     DB.close()
     return records
+
 
 def reportMatch(winner, loser):
     """Records the outcome of a single match between two players.
@@ -90,17 +99,18 @@ def reportMatch(winner, loser):
     DB = connect()
     c = DB.cursor()
     c.execute("INSERT INTO matches VALUES (%s, %s)", (winner, loser))
-    DB.commit() 
+    DB.commit()
     DB.close()
- 
+
+
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
-  
+
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
     to him or her in the standings.
-  
+
     Returns:
       A list of tuples, each of which contains (id1, name1, id2, name2)
         id1: the first player's unique id
@@ -112,9 +122,5 @@ def swissPairings():
     mylist = []
     for i in range(0, len(records), 2):
         mylist.append((records[i][0], records[i][1], records[i+1][0],
-            records[i+1][1]));
+                       records[i+1][1]))
     return mylist
-
-
-
-
